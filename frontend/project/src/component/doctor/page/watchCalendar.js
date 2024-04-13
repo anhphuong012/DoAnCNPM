107108d812b85c908cd8ef593f083cddc61a26a1
Trần Tấn Lộc <20130312@st.hcmuaf.edu.vn>
2024-04-13 13:52:42 +0700

    .
    .

--- frontend/project/src/component/doctor/page/watchCalendar.js
+++ frontend/project/src/component/doctor/page/watchCalendar.js
@@ -11,30 +11,80 @@ export default function WatchCalendar() {
   const [watchList, setWatchList] = useState([
     {
       id: 1,
-
-    }
+      date: "13/04/2024",
+      time: "13:00 - 13:15",
+      phone: "0356560562"
+    },
+    {
+      id: 1,
+      date: "13/04/2024",
+      time: "13:00 - 13:15",
+      phone: "0356560562"
+    },
+    {
+      id: 1,
+      date: "13/04/2024",
+      time: "13:00 - 13:15",
+      phone: "0356560562"
+    },
+    {
+      id: 1,
+      date: "13/04/2024",
+      time: "13:00 - 13:15",
+      phone: "0356560562"
+    },
+    {
+      id: 1,
+      date: "13/04/2024",
+      time: "13:00 - 13:15",
+      phone: "0356560562"
+    },
+    {
+      id: 1,
+      date: "13/04/2024",
+      time: "13:00 - 13:15",
+      phone: "0356560562"
+    },
+    {
+      id: 1,
+      date: "13/04/2024",
+      time: "13:00 - 13:15",
+      phone: "0356560562"
+    },
   ]);
 
   return (
     <div className="bodyWatchCalendar">
       <TabBar/>
       <div className="bodyMain">
-        <div className="titleDiv">
-          <p className="titlePageWatch">Lịch bệnh nhân đã đặt khám</p>
-        </div>
-        <div className="cardDiv">
-          <div className="viewCards">
-            <div className="nameDiv">
-              <p className="titleNameExaminer">Tên bệnh nhân:</p>
-              <p className="nameExaminer"></p>
-            </div>
+        <div className="bodyMainDiv">
+          <div className="titleDiv">
+            <p className="titlePageWatch">Lịch bệnh nhân đã đặt khám:</p>
+          </div>
+          <div className="cardDiv">
+            {watchList.map(appointment => (
+              <div className="viewCards" key={appointment.id}>
+                <div className="nameDiv nameDivBig">
+                  <p className="titleNameExaminer">Tên bệnh nhân:</p>
+                  <p className="nameExaminer">{appointment.name}</p>
+                </div>
+                <div className="nameDiv dateDivMedium">
+                  <p className="titleDateAppointment">Ngày đặt lịch:</p>
+                  <p className="dateAppointment">{appointment.date}</p>
+                </div>
+                <div className="nameDiv timeDivMedium">
+                  <p className="titletimeAppointment">Thời gian khám:</p>
+                  <p className="timeAppointment">{appointment.time}</p>
+                </div>
+                <div className="nameDiv phoneDivMedium">
+                  <p className="titlePhoneAppointment">Số điện thoại người khám:</p>
+                  <p className="phoneAppointment">{appointment.phone}</p>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
     </div>
   )
 }
-
-
-
-
